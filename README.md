e3fc50657e6ea20579bee7ea4e250c324f4fe384
nayeem-17 <nasa.28apr@gmail.com>
2023-11-04 07:16:09 +0600

    add Readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# infra
+# infra code for tax wizard
+## terraform project structure\
+  .
+├── `backend`\
+│   ├── main.tf\
+│   ├── output.tf\
+│   ├── terraform.tfstate\
+│   └── terraform.tfstate.backup\
+├── `ec2`\
+│   ├── aws-test.pem\
+│   ├── backend.tf\
+│   ├── main.tf\
+│   ├── outputs.tf\
+│   ├── public_key.pub\
+│   ├── sonar.tf\
+│   └── variables.tf\
+├── `ecr`\
+│   ├── backend.tf\
+│   └── main.tf\
+├── `ecs`\
+│   ├── alb.tf\
+│   ├── asg.tf\
+│   ├── backend.tf\
+│   ├── data.tf\
+│   ├── ec2.tf\
+│   ├── ecs.sh\
+│   ├── iam.tf\
+│   └── main.tf\
+├── `rds`\
+│   ├── backend.tf\
+│   ├── main.tf\
+│   ├── outputs.tf\
+│   └── variables.tf\
+├── `secret-manager`\
+│   ├── backend.tf\
+│   ├── main.tf\
+│   └── output.tf\
+└── `vpc`\
+|   ├── backend.tf\
+|   ├── main.tf\
+|   └── output.tf\
+├── destroy.sh\
--- backend/main.tf
+++ backend/main.tf
@@ -35,3 +35,51 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+# Create an S3 bucket
+resource "aws_s3_bucket" "terraform_state1" {
+  bucket = "tax-wizard-frontend"
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "enabled_" {
+  bucket = aws_s3_bucket.terraform_state1.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "default_" {
+  bucket = aws_s3_bucket.terraform_state1.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Grant necessary permissions to allow uploading objects to the bucket
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.terraform_state1.bucket
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.terraform_state1.bucket}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
